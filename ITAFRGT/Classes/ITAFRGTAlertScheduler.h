9a6836947cde7b17def310488d2f10cd25649d31
Bryce Hammond <bryce.hammond@imulus.com>
2014-07-16 00:47:45 -0600

    Add scheduler

---
+++ ITAFRGT/Classes/ITAFRGTAlertScheduler.h
@@ -0,0 +1,15 @@
+//
+//  ITAFRGTAlertScheduler.h
+//  ITAFRGT
+//
+//  Created by Bryce Hammond on 7/16/14.
+//  Copyright (c) 2014 Fluidvision Design. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ITAFRGTAlertScheduler : NSObject
+
++ (void)scheduleAlerts;
+
+@end
---
+++ ITAFRGT/Classes/ITAFRGTAlertScheduler.m
@@ -0,0 +1,18 @@
+//
+//  ITAFRGTAlertScheduler.m
+//  ITAFRGT
+//
+//  Created by Bryce Hammond on 7/16/14.
+//  Copyright (c) 2014 Fluidvision Design. All rights reserved.
+//
+
+#import "ITAFRGTAlertScheduler.h"
+
+@implementation ITAFRGTAlertScheduler
+
++ (void)scheduleAlerts
+{
+    
+}
+
+@end
